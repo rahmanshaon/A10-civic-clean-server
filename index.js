9882eac75aa7993b624f1ddb75599816ad254553
rahmanshaon <rahmanshaon45@gmail.com>
2025-11-09 17:12:34 +0600

    mongodb connection setup added

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 // Initialize the Express app
 const app = express();
 const port = process.env.PORT || 3000;
 
 // --- Middlewares ---
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-// --- Simple test route ---
-app.get('/', (req, res) => {
-  res.send('Civic Clean Server is running!');
+// --- MongoDB Connection ---
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pc7o2g1.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-// --- Start the server ---
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// --- Test Route ---
+app.get("/", (req, res) => {
+  res.send("Civic Clean Server is running!");
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
